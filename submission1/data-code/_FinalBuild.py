151e589043eb891f18f1c281450c6576b499daa8
Ilse Novis <ilse.novis@emory.edu>
2025-03-26 11:12:13 -0400

    initial setup

---
+++ submission1/data-code/_FinalBuild.py
@@ -0,0 +1,11 @@
+# Author:        Ilse Novis
+# Date Created:  1/29/2025
+# Date Edited:   1/29/2025
+# Notes:         Python file to build Medicare Advantage dataset for Econ/HLTH 470 Hwk 1
+
+
+# Call individual scripts -------------------------------------------------
+
+exec(open("submission2/data-code/1_Plan_Data.py").read())
+
+exec(open("submission2/data-code/2_SA_Data.py").read())
